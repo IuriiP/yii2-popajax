68eb594833dbe370cb3a5c115bf6bda070a45555
mouse <hardwork.mouse@gmail.com>
2016-01-09 13:48:54 +0200

    Test 0.1.1

---
+++ Popup.php
@@ -0,0 +1,26 @@
+<?php namespace iuriip\yii2popajax;
+
+use Yii;
+use yii\bootstrap\Modal;
+
+class Popajax extends Modal {
+
+    public function run() {
+        parent::run();
+
+        $js = <<<JS
+$(function(){
+    $(".modal-button").click(function(){ 
+        console.log($(this));
+        var target = $(this).attr('modal-target');
+        $(target)
+            .modal("show")
+            .find(".modal-body")
+            .load( $(this).closest("[modal-href]").attr("modal-href") );
+    });
+});                
+JS;
+        $this->registerJs($js, View::POS_READY, 'popajax');
+    }
+
+}
